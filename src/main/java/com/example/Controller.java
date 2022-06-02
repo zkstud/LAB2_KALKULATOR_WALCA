390ed7b0b3ece84730f9c2dd335e5b95de6e7340
ZAKHAR <37652@studentwsei.pl>
2022-06-02 22:13:12 +0200

    Dodalem textField = 3 i przycisk obliczenia

--- src/main/java/com/example/Controller.java
+++ src/main/java/com/example/Controller.java
@@ -1,7 +1,21 @@
 package com.example;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class Controller {
+    @FXML
+    public Button btnOblicz;
+    @FXML
+    public TextField txtPromien;
+    @FXML
+    public TextField txtWysokosc;
+    @FXML
+    public TextField txtGrubosc;
 
+    @FXML
+    public void onActionOblicz(ActionEvent actionEvent) {
+    }
 }
