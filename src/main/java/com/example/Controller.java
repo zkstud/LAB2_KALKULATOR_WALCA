b81388bff486874463cf7ca45e1689f067dbd112
ZAKHAR <37652@studentwsei.pl>
2022-06-06 17:47:51 +0200

    commit: dodalem foldera .idea

--- src/main/java/com/example/Controller.java
+++ src/main/java/com/example/Controller.java
@@ -26,19 +26,23 @@ public class Controller {
 
     @FXML
     public void onActionOblicz(ActionEvent actionEvent) {
-        labelStalWaga.setText(obliczWageWalcaStali(Double.parseDouble(txtSrednica.getText()), Double.parseDouble(txtWysokosc.getText())) + " kg");
-        labelStalKoszt.setText((obliczWageWalcaStali(Double.parseDouble(txtSrednica.getText()), Double.parseDouble(txtWysokosc.getText())) * Double.parseDouble(txtCenaZaKg.getText())) + " zl");
-        labelNierdzWaga.setText(obliczWageWalcaStaliNierdzewnej(Double.parseDouble(txtSrednica.getText()), Double.parseDouble(txtWysokosc.getText())) + " kg");
-        labelNierdzKoszt.setText((obliczWageWalcaStaliNierdzewnej(Double.parseDouble(txtSrednica.getText()), Double.parseDouble(txtWysokosc.getText()))) * Double.parseDouble(txtCenaZaKg.getText()) + " zl");
+        labelStalWaga.setText(Math.round(obliczWageWalcaStali(
+                Double.parseDouble(txtSrednica.getText()), Double.parseDouble(txtWysokosc.getText()))) + " kg");
+        labelStalKoszt.setText(Math.round((obliczWageWalcaStali(
+                Double.parseDouble(txtSrednica.getText()), Double.parseDouble(txtWysokosc.getText())) * Double.parseDouble(txtCenaZaKg.getText()))) + " zl");
+        labelNierdzWaga.setText(Math.round(obliczWageWalcaStaliNierdzewnej(
+                Double.parseDouble(txtSrednica.getText()), Double.parseDouble(txtWysokosc.getText()))) + " kg");
+        labelNierdzKoszt.setText(Math.round(obliczWageWalcaStaliNierdzewnej(
+                Double.parseDouble(txtSrednica.getText()), Double.parseDouble(txtWysokosc.getText())) * Double.parseDouble(txtCenaZaKg.getText())) + " zl");
     }
 
     public double obliczWageWalcaStali(double S, double H) {
-        double V;
+        double V = 0;
         return Math.round(V = ((Math.PI * Math.pow(S, 2) * H) / 4000) * 7.87);
     }
 
     public double obliczWageWalcaStaliNierdzewnej(double S, double H) {
-        double V;
+        double V = 0;
         return Math.round(V = ((Math.PI * Math.pow(S, 2) * H) / 4000) * 8.15);
     }
 }
